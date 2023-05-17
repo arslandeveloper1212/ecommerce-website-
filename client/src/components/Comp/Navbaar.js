dac8c8dd7013234c097298e60e703a39471d2882
Arslan Ahmad <106490352+arslandeveloper1212@users.noreply.github.com>
2023-05-18 01:46:42 +0500

    working

--- client/src/components/Comp/Navbaar.js
+++ client/src/components/Comp/Navbaar.js
@@ -7,8 +7,13 @@ import Stack from '@mui/material/Stack';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import CloseIcon from '@mui/icons-material/Close';
+import { useSelector } from 'react-redux';
 
 const Navbaar = () => {
+
+
+  const { carts } = useSelector((state) => state.cartdata)
+
 
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
@@ -43,7 +48,7 @@ const Navbaar = () => {
               <Link to="/" className='text-decoration-none btn_color_yellow'><h2>Ecommerce</h2></Link>
             </div>
             <div className='right_content d-flex align-items-center gap'>
-              <Badge badgeContent={4} color="primary" style={{ color: "white", fontSize: "25px" }}
+              <Badge badgeContent={carts.length} color="primary" style={{ color: "white", fontSize: "25px" }}
                 id="basic-button"
                 aria-controls={open ? 'basic-menu' : undefined}
                 aria-haspopup="true"
@@ -59,15 +64,54 @@ const Navbaar = () => {
                 anchorEl={anchorEl}
                 open={open}
                 onClose={handleClose}
+                // style= {{width: "55rem"}}
                 MenuListProps={{
                   'aria-labelledby': 'basic-button',
                 }}
               >
-                <div className='cart_details_click' style={{position:"relative", padding: "20px"}}>
-                <CloseIcon style={{position:"absolute", right: "10", top: "6"}} onClick={handleClose} />
-                  <p style={{fontSize: "22px", textAlign: "center"}} >Your cart is empty</p>
-                  <img src='https://bakestudio.in/assets/images/cart/empty-cart.gif' width="300px" alt= "image"/>
-                </div>
+
+                {carts.length ?
+
+                  <div style={{ padding: "10px", width: "24rem" }}>
+                    <div className='badge_inside_card_showing text-center mt-3'>
+                      <h5>Shopping Cart Page</h5>
+                    </div>
+                    <div className='badge_content_controls mt-5 mb-5 px-2'>
+                      {
+                        carts.map((e, index) => {
+                          return (
+                            <div key={index} className='d-flex align-items-center'>
+                              <img src={e.image} alt='title' className='gap' style={{ width: "100px", paddingRight: "13px" }} />
+                              <div className='d-flex flex-column'>
+                                <h5 style={{ fontSize: "15px" }}>{e.title.shortTitle}</h5>
+                                <h5 style={{ fontSize: "20px" }} >{e.price}</h5>
+                                
+                               
+                              </div>
+                            </div>
+                          )
+                        })
+                      }
+                    </div>
+
+
+                  </div>
+
+
+                  :
+
+                  <div>
+                    <div className='cart_details_click' style={{ position: "relative", padding: "20px" }}>
+                      <CloseIcon style={{ position: "absolute", right: "10", top: "6" }} onClick={handleClose} />
+                      <p style={{ fontSize: "22px", textAlign: "center" }} >Your cart is empty</p>
+                      <img src='https://bakestudio.in/assets/images/cart/empty-cart.gif' width="300px" alt="image" />
+                    </div>
+                  </div>}
+
+
+
+
+
               </Menu>
 
 
